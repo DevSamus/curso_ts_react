f90060c53e35f7c19b22ed8b52e67c64a332d71a
Samuel Pinho A <devsamus@gmail.com>
2022-06-12 10:31:24 -0300

    prime commit

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import styles from "./App.module.css"
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Modal from './components/Modal'
+import TaskForm from './components/TaskForm'
+import TaskList from './components/TaskList'
+import { ITask } from './interfaces/ITask'
+
+function App() {
+	const [taskList, setTaskList] = useState<ITask[]>([])
+	const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null)
+
+	const deleteTask = (id: number) => setTaskList(taskList.filter(task => task.id !== id))
+	const hideOrShowModal = (display: boolean) => {
+		const modal = document.querySelector("#modal")
+		if (display) modal!.classList.remove('hide')
+		else modal!.classList.add('hide')
+	}
+
+	const editTask = (task: ITask): void => {
+		hideOrShowModal(true)
+		setTaskToUpdate(task)
+	}
+
+	return (
+		<div >
+			<Modal children={<TaskForm btnText='Editar' taskList={taskList} task={taskToUpdate} />} />
+			<Header />
+			<main className={styles.main}>
+				<div >
+					<h2>O que você vai fazer?</h2>
+					<TaskForm btnText='Criar Tarefa' 
+					          taskList={taskList} 										
+										setTaskList={setTaskList} />
+				</div>
+				<div >
+					<h2>Suas tarefas:</h2>
+					<TaskList taskList={taskList} handleDelete={deleteTask} handleEdit={editTask} />
+				</div>
+			</main>
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/TaskForm.tsx
@@ -0,0 +1,71 @@
+import React, { useState, ChangeEvent, FormEvent, useEffect } from "react";
+import styles from "./TaskForm.module.css";
+import { ITask } from '../interfaces/ITask';
+
+type Props = {
+  btnText: string;
+	taskList:ITask[]
+	setTaskList?:React.Dispatch<React.SetStateAction<ITask[]>>
+	task?:ITask | null
+};
+
+const TaskForm = ({btnText, taskList, setTaskList, task }: Props) => {
+  const [id, setId] = useState<number>();
+  const [title, setTitle] = useState<string>("");
+  const [difficulty, setDifficulty] = useState<number>(0);
+
+	useEffect (()=> {
+		if(task) {
+			setId(task.id)
+			setTitle(task.title)
+			setDifficulty(task.difficulty)
+		}
+
+	},[task])
+
+  const addTaskHandler = (e:FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const id = Math.floor(Math.random()*1000);
+    const newTask:ITask ={id, title, difficulty}
+		setTaskList!([...taskList,newTask]); //# ! para objetos que podem ser nulos argumento opcional
+		setTitle("")
+		setDifficulty(0);
+		console.log(taskList);
+		
+	};
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.name === "title") setTitle(e.target.value);
+    else setDifficulty(parseInt(e.target.value));
+    console.log(title);
+    console.log(difficulty);
+  };
+
+  return (
+    <form onSubmit={addTaskHandler} className={styles.form}>
+      <div className={styles.input_container}>
+        <label htmlFor="title">Título:</label>
+        <input
+          type="text"
+          name="title"
+          placeholder="Título da tarefa"
+          onChange={handleChange}
+					value={title}
+        />
+      </div>
+      <div className={styles.input_container}>
+        <label htmlFor="difficulty">Dificuldade:</label>
+        <input
+          type="text"
+          name="difficulty"
+          placeholder="Dificuldade da tarefa"
+					onChange={handleChange}
+					value={difficulty}
+        />
+      </div>
+      <input type="submit" value={btnText}  />
+    </form>
+  );
+};
+
+export default TaskForm;
